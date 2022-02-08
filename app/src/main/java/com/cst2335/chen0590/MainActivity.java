7c7d476a38195135178e0ae708cb5998f2a9db05
Meiping Chen <chen0590@algonquinlive.com>
2022-02-08 10:44:57 -0500

    commit

--- app/src/main/java/com/cst2335/chen0590/MainActivity.java
+++ app/src/main/java/com/cst2335/chen0590/MainActivity.java
@@ -22,34 +22,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.email_login);
 
 
-        Button btn = (Button) findViewById(R.id.button2);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String toastMessage = MainActivity.this.getResources().getString(R.string.toast_message);
-                Toast.makeText(MainActivity.this, toastMessage, Toast.LENGTH_LONG).show();
-
-            }
-        });
-
-        Switch mySwitch = (Switch) findViewById(R.id.switch1);
-        String switchNow = MainActivity.this.getResources().getString(R.string.switch_now);
-        String undo = MainActivity.this.getResources().getString(R.string.undo);
-
-        mySwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton cb, boolean b) {
-                if (b == true) {
-                    String on = MainActivity.this.getResources().getString(R.string.on);
-                    Snackbar.make(btn, switchNow + on, Snackbar.LENGTH_LONG).setAction( undo, click -> cb.setChecked(!b)).show();
-
-                } else if (b == false) {
-                    String off = MainActivity.this.getResources().getString(R.string.off);
-                    Snackbar.make(btn, switchNow + off, Snackbar.LENGTH_LONG).setAction( undo, click -> cb.setChecked(!b)).show();
-                }
-
-            }
-        });
 
 
     }
