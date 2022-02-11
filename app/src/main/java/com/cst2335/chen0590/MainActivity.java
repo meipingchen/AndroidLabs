ff7ddae6beea060add0349d1ad7c91072a38a5cb
Meiping Chen <chen0590@algonquinlive.com>
2022-02-10 20:21:37 -0500

    commit

--- app/src/main/java/com/cst2335/chen0590/MainActivity.java
+++ app/src/main/java/com/cst2335/chen0590/MainActivity.java
@@ -21,9 +21,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_linear);
-
-    );
+        setContentView(R.layout.activity_main_relative);
 
         Button btn = (Button) findViewById(R.id.button2);
         btn.setOnClickListener(new View.OnClickListener() {
